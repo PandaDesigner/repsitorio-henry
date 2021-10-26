dd00f6725e81da5f6011a6a70df379ad0f50b5c0
PandaDesigner <pedro@pandadesigners.com>
2021-10-26 17:43:52 -0500

    iniciando prueba

---
+++ archivo-ejemplo.js
@@ -0,0 +1,7 @@
+let numero = 0;
+for (let i = 0; i < resultadosTest.length; i++) {
+  numero = numero + resultadosTest[i]; 
+}
+return numero / resultadosTest.length;
+}
+///spread operator javascript
